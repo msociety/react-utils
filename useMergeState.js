8551bb8e24935f8d18e857d554123655a718eb9e
Miguel Molina <miguelmolina.tk@gmail.com>
2021-07-26 15:51:50 +0200

    Create useMergeState.js

---
+++ useMergeState.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const useMergeState = (initial = {}) => {
+  const [state, set] = useState(initial);
+  return [
+    state,
+    updater => set(prev => (typeof updater === "function" ? { ...prev, ...updater(prev) } : { ...prev, ...updater })),
+  ];
+};
+
+export default useMergeState;
